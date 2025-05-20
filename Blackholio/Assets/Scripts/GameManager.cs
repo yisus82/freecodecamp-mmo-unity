cef15189f79b124ae98382cf286ac187c1cc8673
yisus82 <djalma8@gmail.com>
2025-05-20 10:40:29 +0200

    Connect to SpaceTimeDB

--- Blackholio/Assets/Scripts/GameManager.cs
+++ Blackholio/Assets/Scripts/GameManager.cs
@@ -1,16 +1,92 @@
+using System;
+using SpacetimeDB;
+using SpacetimeDB.Types;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private const string SERVER_URL = "http://127.0.0.1:3000";
+    private const string MODULE_NAME = "blackholio";
+
+    public static event Action OnConnected;
+    public static event Action OnSubscriptionApplied;
+
+    public float borderThickness = 2;
+    public Material borderMaterial;
+
+    public static GameManager Instance { get; private set; }
+    public static Identity LocalIdentity { get; private set; }
+    public static DbConnection Conn { get; private set; }
+
+    private void Start()
     {
-        
+        Instance = this;
+        Application.targetFrameRate = 60;
+
+        // In order to build a connection to SpacetimeDB we need to register
+        // our callbacks and specify a SpacetimeDB server URI and module name.
+        var builder = DbConnection.Builder()
+            .OnConnect(HandleConnect)
+            .OnConnectError(HandleConnectError)
+            .OnDisconnect(HandleDisconnect)
+            .WithUri(SERVER_URL)
+            .WithModuleName(MODULE_NAME);
+
+        // If the user has a SpacetimeDB auth token stored in the Unity PlayerPrefs,
+        // we can use it to authenticate the connection.
+		if (AuthToken.Token != "")
+        {
+            builder = builder.WithToken(AuthToken.Token);
+        }
+
+        // Building the connection will establish a connection to the SpacetimeDB
+        // server.
+        Conn = builder.Build();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called when we connect to SpacetimeDB and receive our client identity
+    private void HandleConnect(DbConnection conn, Identity identity, string token)
     {
-        
+        Debug.Log("Connected.");
+        AuthToken.SaveToken(token);
+        LocalIdentity = identity;
+
+        OnConnected?.Invoke();
+
+        // Request all tables
+        Conn.SubscriptionBuilder()
+            .OnApplied(HandleSubscriptionApplied)
+            .SubscribeToAllTables();
+    }
+
+    private void HandleConnectError(Exception ex)
+    {
+        Debug.LogError($"Connection error: {ex}");
+    }
+
+    private void HandleDisconnect(DbConnection _conn, Exception ex)
+    {
+        Debug.Log("Disconnected.");
+        if (ex != null)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    private void HandleSubscriptionApplied(SubscriptionEventContext ctx)
+    {
+        Debug.Log("Subscription applied!");
+        OnSubscriptionApplied?.Invoke();
+    }
+
+    public static bool IsConnected()
+    {
+        return Conn != null && Conn.IsActive;
+    }
+
+    public void Disconnect()
+    {
+        Conn.Disconnect();
+        Conn = null;
     }
 }
--- Server/Lib.cs
+++ Server/Lib.cs
@@ -2,30 +2,64 @@ using SpacetimeDB;
 
 public static partial class Module
 {
-    [SpacetimeDB.Table]
-    public partial struct Person
+    // We're using this table as a singleton, so in this table
+    // there only be one element where the `id` is 0.
+    [Table(Name = "config", Public = true)]
+    public partial struct Config
     {
-        [SpacetimeDB.AutoInc]
-        [SpacetimeDB.PrimaryKey]
-        public int Id;
-        public string Name;
-        public int Age;
+        [PrimaryKey]
+        public uint id;
+        public ulong world_size;
     }
 
-    [SpacetimeDB.Reducer]
-    public static void Add(ReducerContext ctx, string name, int age)
+    [Type]
+    public partial struct DbVector2(float x, float y)
     {
-        var person = ctx.Db.Person.Insert(new Person { Name = name, Age = age });
-        Log.Info($"Inserted {person.Name} under #{person.Id}");
+        public float x = x;
+        public float y = y;
     }
 
-    [SpacetimeDB.Reducer]
-    public static void SayHello(ReducerContext ctx)
+    [Table(Name = "entity", Public = true)]
+    public partial struct Entity
     {
-        foreach (var person in ctx.Db.Person.Iter())
-        {
-            Log.Info($"Hello, {person.Name}!");
-        }
-        Log.Info("Hello, World!");
+        [PrimaryKey, AutoInc]
+        public uint entity_id;
+        public DbVector2 position;
+        public uint mass;
+    }
+
+    [Table(Name = "circle", Public = true)]
+    public partial struct Circle
+    {
+        [PrimaryKey]
+        public uint entity_id;
+        [SpacetimeDB.Index.BTree]
+        public uint player_id;
+        public DbVector2 direction;
+        public float speed;
+        public ulong last_split_time;
+    }
+
+    [Table(Name = "food", Public = true)]
+    public partial struct Food
+    {
+        [PrimaryKey]
+        public uint entity_id;
+    }
+
+    [Table(Name = "player", Public = true)]
+    public partial struct Player
+    {
+        [PrimaryKey]
+        public Identity identity;
+        [Unique, AutoInc]
+        public uint player_id;
+        public string name;
+    }
+
+    [Reducer(ReducerKind.ClientConnected)]
+    public static void Debug(ReducerContext ctx)
+    {
+        Log.Info($"{ctx.Sender} just connected to the server.");
     }
 }
